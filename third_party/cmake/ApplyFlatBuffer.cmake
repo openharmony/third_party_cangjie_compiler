94a8a6f37ff1d40d37b758f84ed626fd93ee0a0d
xuanjz <xuanjiazhen@huawei.com>
2025-10-07 18:56:14 +0800

    docs: add beta desc
    Signed-off-by: xuanjz <xuanjiazhen@huawei.com>

--- third_party/cmake/ApplyFlatBuffer.cmake
+++ third_party/cmake/ApplyFlatBuffer.cmake
@@ -4,6 +4,8 @@
 #
 # See https://cangjie-lang.cn/pages/LICENSE for license information.
 
+# The Cangjie API is in Beta. For details on its capabilities and limitations, please refer to the README file.
+
 set(FLATBUFFERS_SOURCE_DIR ${CMAKE_ARGV3})
 set(FLATBUFFERS_PATCH ${CMAKE_ARGV4})
 
